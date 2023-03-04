750e93fff7f8a0ee19821e9e4d00f276b0d2c5d4
Homura-san <erickbispo@ufba.br>
2023-03-04 19:19:29 -0300

    Types e Interfaces

--- index.ts
+++ index.ts
@@ -1,7 +1,22 @@
-console.log('TypeScript');
+// Types
+// Interfaces
 
-function soma(a: number, b: number) {
-    return a + b;
+interface Ianimal {
+    nome: string;
+    tipo: 'terrestre' | 'aquatico';
+    executarRugido(alturaEmDecibeis: number): void;
 }
 
-soma(8, 4)
+interface Ifelino extends Ianimal {
+    visaoNoturna: boolean;
+}
+
+const animal: Ianimal = {
+    nome: 'Elefante',
+    tipo: 'aquatico',
+    executarRugido: (alturaEmDecibeis) => (`${alturaEmDecibeis}dB`)
+}
+
+// console.log(animal.executarRugido(5));
+
+
