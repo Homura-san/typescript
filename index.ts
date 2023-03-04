cf7070c2c359d9918419069bc10b05d39dd844be
Homura-san <erickbispo@ufba.br>
2023-03-04 19:32:52 -0300

    Types

--- index.ts
+++ index.ts
@@ -4,19 +4,33 @@
 interface Ianimal {
     nome: string;
     tipo: 'terrestre' | 'aquatico';
-    executarRugido(alturaEmDecibeis: number): void;
+    domestico: boolean;
 }
+
+
 
 interface Ifelino extends Ianimal {
     visaoNoturna: boolean;
 }
 
-const animal: Ianimal = {
-    nome: 'Elefante',
-    tipo: 'aquatico',
-    executarRugido: (alturaEmDecibeis) => (`${alturaEmDecibeis}dB`)
+interface ICanino extends Ianimal {
+    porte: 'pequeno' | 'medio' | 'grande';
+}
+
+type IDomestico = Ifelino | ICanino;
+
+const animal: IDomestico = {
+    domestico: true,
+    nome: 'Cachorro',
+    porte: 'medio',
+    tipo: 'terrestre'
 }
 
 // console.log(animal.executarRugido(5));
 
 
+// type Ianimal = {
+//     nome: string;
+//     tipo: 'terrestre' | 'aquatico';
+//     executarRugido(alturaEmDecibeis: number): void;
+// }
