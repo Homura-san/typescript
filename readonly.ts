f21aaedbd130328a0327520b281cc69260494161
Homura-san <erickbispo@ufba.br>
2023-03-09 11:11:18 -0300

    Readonly e private

---
+++ readonly.ts
@@ -0,0 +1,27 @@
+interface Cachorro {
+    readonly nome: string,
+    readonly idade: number,
+    readonly parqueFavorito?: string
+}
+
+type CachorroSomenteLeitura = {
+    readonly [K in keyof Cachorro] - ?: Cachorro[K]
+    // - ? remove parâmetros opcionais
+}
+
+class MeuCachorro implements CachorroSomenteLeitura {
+    nome;
+    idade;
+    parqueFavorito;
+    
+    constructor(nome: string, idade: number, parqueFavorito: string){
+        this.nome = nome;
+        this.idade = idade;
+        this.parqueFavorito = parqueFavorito;
+    }
+}
+
+const cao = new MeuCachorro('Apollo', 14, 'Verde');
+cao.idade = 8
+
+console.log(cao)
